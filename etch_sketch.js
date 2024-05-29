a248581d294d08b844f5727dabca859afbc7b8e8
expverhno3 <flyed666@outlook.com>
2024-05-29 15:23:26 +0800

    add button, clear and create new grids

--- etch_sketch.js
+++ etch_sketch.js
@@ -1,5 +1,39 @@
+
+function readGridSize() {
+    let size = parseInt(prompt("Please enter the number of squares per side."))
+    if (size && size > 0 && size<100) {
+        return size
+    } else{
+        alert("please enter a valid number!")
+    }
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function createGrid(size) {
+    let totalNumber = size * size
+    let squareHeight = 96 / size
+    const container = document.querySelector('.container');
+    removeAllChildNodes(container)
+    for (let i=0; i < totalNumber; i++) {
+        const item = document.createElement("div")
+        item.classList.add("user-square")
+        item.style.width = `${squareHeight}vw`
+        item.style.height = `${squareHeight}vw`
+        item.style.border = "1px solid #000"
+        item.style.boxSizing = "border-box"
+        container.appendChild(item)
+    }
+    
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     const container = document.querySelector('.container');
+    const gridSizeButton = document.querySelector("button.set-size");
 
     for (let i = 0; i < 256; i++) {
         const div = document.createElement('div');
@@ -7,9 +41,14 @@ document.addEventListener("DOMContentLoaded", function() {
         container.appendChild(div);
     }
     container.addEventListener('mouseover', function(e) {
-        if (e.target.classList.contains('square')) {
+        if (e.target.classList.contains('square') || e.target.classList.contains("user-square")) {
             e.target.style.backgroundColor = '#3498db'; // Change this color to whatever you want
         }
     });
+    
+    gridSizeButton.addEventListener("click", () =>{
+        let size = readGridSize()
+        createGrid(size)
+    })
 });
 
